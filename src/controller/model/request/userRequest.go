f586c64e33fa79d7fa807cd73e4d4219251fa942
ncsmatias <ncs.matias@gmail.com>
2023-10-26 08:07:49 -0300

    feat: create validation request fields

--- src/controller/model/request/userRequest.go
+++ src/controller/model/request/userRequest.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Admin    bool   `json:"admin"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,containsany=!@$#*"`
+	Name     string `json:"name" binding:"max=128"`
+	Admin    bool   `json:"admin" binding:"boolean"`
 }
