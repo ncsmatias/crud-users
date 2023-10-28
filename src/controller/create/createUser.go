a6f6d04c0b0978e34d3461dd92383b759d0fa2d7
ncsmatias <ncs.matias@gmail.com>
2023-10-28 15:49:10 -0300

    feat: create info and errors logs

--- src/controller/create/createUser.go
+++ src/controller/create/createUser.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ncsmatias/crud-users/src/configuration/logger"
 	"github.com/ncsmatias/crud-users/src/configuration/validation"
 	"github.com/ncsmatias/crud-users/src/controller/model/request"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
+	journey := zap.String("journey", "create user")
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to create a new user", err, journey)
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
+	logger.Info("New user created", journey)
 	fmt.Println(userRequest)
 
 }
