845bebcc9c4b01a7650a2140f98735c39a8cb721
ncsmatias <ncs.matias@gmail.com>
2023-10-29 23:55:30 -0300

    feat: add new column

--- db/migration/000002_create_institutions_table.up.sql
+++ db/migration/000002_create_institutions_table.up.sql
@@ -1,5 +1,6 @@
 create table "institutions"(
   "institution_id" uuid primary key default uuid_generate_v4(),
+  "institution_type" varchar not null,
   "name" varchar not null,
   "phone" varchar,
   "address_id" uuid REFERENCES addresses ("address_id"),
