aa83d65533ce23aec180d81c4dc60eda3f512a54
ncsmatias <ncs.matias@gmail.com>
2023-11-04 10:35:52 -0300

    feat: include address fields

--- db/migration/000002_create_institutions_table.up.sql
+++ db/migration/000002_create_institutions_table.up.sql
@@ -3,6 +3,14 @@ create table "institutions"(
   "institution_type" varchar not null,
   "name" varchar not null,
   "phone" varchar,
+  "zip_code" varchar,
+  "street" varchar,
+  "number" varchar,
+  "city" varchar,
+  "state" varchar,
+  "UF" varchar,
+  "country" varchar,
+  "country_code" varchar,
   "address_id" uuid REFERENCES addresses ("address_id"),
   "created_at" timestamptz not null default (now())
 );
